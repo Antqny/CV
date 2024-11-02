06eca89f78cfd7065678e5888fd953db9628a230
Anton Morozov <anmov7@gmail.com>
2024-11-02 18:27:01 +0300

    Изменено фото, стили и скрипт

--- js.js
+++ js.js
@@ -3,5 +3,5 @@ var button = document.querySelector('button');
 
 /*Присваиваем кнопке оброботчик, который ждёт клика и тогда запускает логику */
 button.addEventListener('click', function() {
-    alert('ГООООООЛ!')
+    alert('Неважно, сколько дней в твоей жизни. Важно, сколько жизни в твоих днях')
 })
