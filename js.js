15bf55de85bffafe774360987d5e62796f5697fe
Anton Morozov <anmov7@gmail.com>
2024-11-02 17:32:37 +0300

    Загрузка имеющихся фалов в git

---
+++ js.js
@@ -0,0 +1,7 @@
+/* Объявление переменной на кнопку */
+var button = document.querySelector('button');
+
+/*Присваиваем кнопке оброботчик, который ждёт клика и тогда запускает логику */
+button.addEventListener('click', function() {
+    alert('ГООООООЛ!')
+})
